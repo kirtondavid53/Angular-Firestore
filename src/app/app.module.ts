993b1ab09ce104e8273732b944f278e4ecc6302f
David Kirton <kirtondavid53@gmail.com>
2020-07-05 17:13:28 -0500

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCloKt0AvQz9WgdcNAKtVaz-YIbL4jYkQk",
+    authDomain: "fs1prod-4693e.firebaseapp.com",
+    databaseURL: "https://fs1prod-4693e.firebaseio.com",
+    projectId: "fs1prod-4693e",
+    storageBucket: "fs1prod-4693e.appspot.com",
+    messagingSenderId: "295066881735",
+    appId: "1:295066881735:web:648dd1646a1034972c78e2",
+    measurementId: "G-FDR9S9Q360"
+  }
 };
